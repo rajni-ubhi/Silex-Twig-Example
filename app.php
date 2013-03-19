d253db306f022e9e84c22aee5056dd55c3b06f73
Jackie Xu <jackie@starapple.nl>
2013-03-19 12:20:53 +0100

    Created example

---
+++ app.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views'
+));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html');
+});
+
+$app->get('/{variable}', function ($variable) use ($app) {
+    return $app['twig']->render('index.html', array('variable' => $variable));
+});
+
+$app->run();
